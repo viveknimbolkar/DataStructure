dce105aaf01b3b552dc8d17e71d2ddc63dcc3857
viveknimbolkar <viveknimbolkar50@gmail.com>
2021-05-31 18:52:56 +0530

    add algos

--- Array/Test.java
+++ Array/Test.java
@@ -1,15 +1,39 @@
 package Array;
 
+import Algorithms.InsertionSort;
 /**
  * @author vivek nimbolkar
  *
  */
 public class Test {
 
+	public void insertionSort(int[] arr){
+
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+
+			int key = arr[i];
+			int j = i -1;
+
+			while (j >= 0 && arr[j] > key ){
+				arr[j+1] = arr[j];
+				j--;
+			}
+
+			arr[j+1] = key;
+		}
+	}
+
 	public static void main(String[] args) {
-		int x = 2;
-		x = x%9;
-		System.out.println(x);
+		Test t = new Test();
+		InsertionSort is = new InsertionSort();
+
+		int[] a = {34,32,7,3,76,9,5,87,3,85,465,4,6,2,6,5};
+
+		t.insertionSort(a);
+		is.printSortedArray(a);
+
 	}
 
 }
--- Linked_List/ReverseLinkedList.java
+++
@@ -1,4 +0,0 @@
-package Linked_List;
-
-public class ReverseLinkedList {
-}
