ed2f1a524fa83bf32b95f2f8c9b9f064c1e5b93d
viveknimbolkar <viveknimbolkar50@gmail.com>
2024-02-25 01:18:09 +0530

    update problems 3

---
+++ array/problems/LongestSuccessiveElements.java
@@ -0,0 +1,57 @@
+package array.problems;
+
+import java.util.Arrays;
+
+public class LongestSuccessiveElements {
+
+    boolean linearSearch(int[] a, int element){
+        for(int x : a)
+            if(x == element)
+                return true;
+        return false;
+    }
+
+    //brute force: T=(N*N) S=(1)
+    int findConsecutiveLength(int[] arr){
+        int longest = 1;
+
+        for(int i = 0; i < arr.length; i++){
+            int x = arr[i];
+            int counter = 1;
+            // keep looking x+i number if present in array
+            while(linearSearch(arr,x+1) == true){
+                x = x+1;
+                counter = counter + 1;
+            }
+            longest = Math.max(longest,counter);
+        }
+
+        return longest;
+    }
+
+    // better : by sorting array
+    // T=O(NlogN) S=O(1)
+    int findLength(int[] a){
+        Arrays.sort(a);
+        int longest = 1, lastSmaller = Integer.MIN_VALUE, counter = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if(a[i]-1 == lastSmaller){
+                counter = counter + 1;
+                lastSmaller = a[i];
+            }else if(a[i] == lastSmaller){
+                continue;
+            }else if(a[i] != lastSmaller){
+                counter = 1;
+                lastSmaller = a[i];
+            }
+            longest= Math.max(longest,counter);
+        }
+        return  longest;
+    }
+    public static void main(String[] args) {
+        LongestSuccessiveElements longestElement = new LongestSuccessiveElements();
+        int[] arr = {5,8,3,2,1,4};
+        System.out.println(longestElement.findLength(arr));
+    }
+}
